e2744d1904b4f94dcbeca093f94fa47a72a6e5ae
kike Madrigal <kike@Mac-mini-de-kike.local>
2021-02-07 04:56:42 +0100

    Creada carpeta mac en tools/sjasmplus

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #java -jar  tools/glass/glass-0.5.jar src/main.asm main.bin main.lst
 # ./tools/sjasmplus/sjasmplus --raw=main.com --sym=main.sym --lst=main.lst src/main.asm 
-./tools/sjasmplus/sjasmplus --raw=main.bin  --sym=main.sym --lst=main.lst src/main.asm 
+./tools/sjasmplus/mac/sjasmplus --raw=main.bin  --sym=main.sym --lst=main.lst src/main.asm 
 # mv main.com obj/main.com
 mv main.bin obj/main.bin
 mv main.lst obj/main.lst
